91bad7e92d1ebb58fa992b203995747a8e4160f3
86137 <2532016091@qq.com>
2023-03-05 10:37:32 +0800

    替换、新增copyright，删除空import

--- flowlong-core/src/test/java/test/custom/TestCustomClass.java
+++ flowlong-core/src/test/java/test/custom/TestCustomClass.java
@@ -1,4 +1,4 @@
-/* Copyright 2013-2015 www.snakerflow.com.
+/* Copyright 2023-2025 jobob@qq.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
