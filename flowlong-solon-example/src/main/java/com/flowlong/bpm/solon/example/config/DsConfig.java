9889d24db36f0e2a2be6d8e1e9717806c33b7f13
noear <noear@live.cn>
2023-09-25 10:19:20 +0800

    简化 solon 适配

--- flowlong-solon-example/src/main/java/com/flowlong/bpm/solon/example/config/DsConfig.java
+++ flowlong-solon-example/src/main/java/com/flowlong/bpm/solon/example/config/DsConfig.java
@@ -4,21 +4,14 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
-import org.noear.solon.scheduling.scheduled.manager.IJobManager;
-import org.noear.solon.scheduling.simple.JobManager;
 
 import javax.sql.DataSource;
 
 
 @Configuration
-public class AppConfig {
+public class DsConfig {
     @Bean(name = "flowlong")
     public DataSource ds(@Inject("${flowlong.datasource}") HikariDataSource ds) {
         return ds;
-    }
-
-    @Bean
-    public IJobManager jobManager(){
-        return JobManager.getInstance();
     }
 }
