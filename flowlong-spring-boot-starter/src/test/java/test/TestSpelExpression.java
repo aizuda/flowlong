5207deb0053f4ba2adf31da1dc4a196a9f50b35c
hubin <243194995@qq.com>
2023-11-08 08:44:20 +0800

    节点条件修改为表达式组

--- flowlong-spring-boot-starter/src/test/java/test/TestSpelExpression.java
+++ flowlong-spring-boot-starter/src/test/java/test/TestSpelExpression.java
@@ -1,9 +1,11 @@
 package test;
 
+import com.flowlong.bpm.engine.model.NodeExpression;
 import com.flowlong.bpm.spring.adaptive.SpelExpression;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +16,11 @@ public class TestSpelExpression {
         SpelExpression expression = new SpelExpression();
         Map<String, Object> args = new HashMap<>();
         args.put("day", 8);
-        Assertions.assertTrue(expression.eval(Boolean.class, "#day>7", args));
+        NodeExpression nodeExpression = new NodeExpression();
+        nodeExpression.setLabel("日期");
+        nodeExpression.setField("day");
+        nodeExpression.setOperator(">");
+        nodeExpression.setValue("7");
+        Assertions.assertTrue(expression.eval(Arrays.asList(Arrays.asList(nodeExpression)), args));
     }
 }
