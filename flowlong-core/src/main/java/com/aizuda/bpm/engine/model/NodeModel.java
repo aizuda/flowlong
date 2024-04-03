d6b5476d0c93baf7076bb75238417766cac99d74
hubin <jobob@qq.com>
2024-04-03 22:41:29 +0800

    新增允许选择审批人员属性

--- flowlong-core/src/main/java/com/aizuda/bpm/engine/model/NodeModel.java
+++ flowlong-core/src/main/java/com/aizuda/bpm/engine/model/NodeModel.java
@@ -136,6 +136,10 @@ public class NodeModel implements ModelInstance, Serializable {
      */
     private Boolean userSelectFlag;
     /**
+     * 允许选择审批人员
+     */
+    private Boolean allowSelection;
+    /**
      * 允许转交
      */
     private Boolean allowTransfer;
