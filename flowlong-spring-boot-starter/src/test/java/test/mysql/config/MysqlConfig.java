6957b5744c6870c874ae4b8faea039759495fd63
songyinyin <song_yinyin@163.com>
2024-04-04 17:14:32 +0800

    测试用例增加事务

--- flowlong-spring-boot-starter/src/test/java/test/mysql/config/MysqlConfig.java
+++ flowlong-spring-boot-starter/src/test/java/test/mysql/config/MysqlConfig.java
@@ -11,8 +11,15 @@ import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -40,5 +47,19 @@ public class MysqlConfig extends FlowLongAutoConfiguration {
         sqlSessionFactory.setPlugins(mybatisPlusInterceptor);
         sqlSessionFactory.setConfiguration(configuration);
         return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(TransactionManager.class)
+    DataSourceTransactionManager transactionManager(Environment environment, DataSource dataSource,
+                                                    ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+        DataSourceTransactionManager transactionManager = createTransactionManager(environment, dataSource);
+        transactionManagerCustomizers.ifAvailable((customizers) -> customizers.customize(transactionManager));
+        return transactionManager;
+    }
+
+    private DataSourceTransactionManager createTransactionManager(Environment environment, DataSource dataSource) {
+        return environment.getProperty("spring.dao.exceptiontranslation.enabled", Boolean.class, Boolean.TRUE)
+                ? new JdbcTransactionManager(dataSource) : new DataSourceTransactionManager(dataSource);
     }
 }
