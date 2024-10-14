a864b76b958126f1547c14bf6c3a76c29ac1d31d
hubin <jobob@qq.com>
2024-10-14 11:18:06 +0800

    新增自动认领角色测试

---
+++ flowlong-spring-boot-starter/src/test/java/test/mysql/TestAutoClaimRole.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023-2025 Licensed under the AGPL License
+ */
+package test.mysql;
+
+import com.aizuda.bpm.engine.entity.FlwHisInstance;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试自动认领角色审批
+ */
+@Slf4j
+public class TestAutoClaimRole extends MysqlTest {
+
+    @BeforeEach
+    public void before() {
+        processId = this.deployByResource("test/testAutoClaimRole.json", testCreator);
+    }
+
+    @Test
+    public void test() {
+        flowLongEngine.startInstanceById(processId, testCreator).ifPresent(instance -> {
+
+            // 先认领角色（假设 test02 为项目经理）
+            this.executeActiveTasks(instance.getId(), t -> this.flowLongEngine.taskService().claimRole(t.getId(), test2Creator));
+
+            // 执行任务
+            executeActiveTasks(instance.getId(), test2Creator);
+
+            FlwHisInstance hisInstance = flowLongEngine.queryService().getHistInstance(instance.getId());
+            Assertions.assertEquals(1, hisInstance.getInstanceState());
+        });
+    }
+}
--- flowlong-spring-boot-starter/src/test/java/test/mysql/config/TestTaskActorProvider.java
+++ flowlong-spring-boot-starter/src/test/java/test/mysql/config/TestTaskActorProvider.java
@@ -5,9 +5,11 @@ import com.aizuda.bpm.engine.core.FlowCreator;
 import com.aizuda.bpm.engine.core.enums.NodeSetType;
 import com.aizuda.bpm.engine.entity.FlwTaskActor;
 import com.aizuda.bpm.engine.impl.GeneralTaskActorProvider;
+import com.aizuda.bpm.engine.model.NodeAssignee;
 import com.aizuda.bpm.engine.model.NodeModel;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,12 +20,20 @@ public class TestTaskActorProvider extends GeneralTaskActorProvider {
     @Override
     public List<FlwTaskActor> getTaskActors(NodeModel nodeModel, Execution execution) {
         if (NodeSetType.role.eq(nodeModel.getSetType())) {
+            NodeAssignee nodeAssignee = nodeModel.getNodeAssigneeList().get(0);
 
             // 测试用例 TestAutoSkip 测试角色自动分配处理人员
-            if ("100100".equals(nodeModel.getNodeAssigneeList().get(0).getId())) {
+            if ("100100".equals(nodeAssignee.getId())) {
                 return Arrays.asList(
                         FlwTaskActor.ofFlowCreator(FlowCreator.of("test001", "测试001")),
                         FlwTaskActor.ofFlowCreator(FlowCreator.of("test002", "测试002"))
+                );
+            }
+
+            // 测试用例 TestAutoClaimRole 测试自动认领角色审批
+            if ("100200".equals(nodeAssignee.getId())) {
+                return Collections.singletonList(
+                        FlwTaskActor.ofRole(nodeAssignee.getTenantId(), nodeAssignee.getId(), nodeAssignee.getName())
                 );
             }
         }
