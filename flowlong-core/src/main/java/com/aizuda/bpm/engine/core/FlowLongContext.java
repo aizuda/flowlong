c867329f9f67c6456f2da32154cd72abb5e8840a
hubin <jobob@qq.com>
2025-05-02 08:56:19 +0800

    发布 1.1.8 版本

--- flowlong-core/src/main/java/com/aizuda/bpm/engine/core/FlowLongContext.java
+++ flowlong-core/src/main/java/com/aizuda/bpm/engine/core/FlowLongContext.java
@@ -193,7 +193,7 @@ public class FlowLongContext {
         if (banner) {
             System.out.println("┌─┐┬  ┌─┐┬ ┬┬  ┌─┐┌┐┌┌─┐");
             System.out.println("├┤ │  │ │││││  │ │││││ ┬");
-            System.out.println("└  ┴─┘└─┘└┴┘┴─┘└─┘┘└┘└─┘  1.1.7");
+            System.out.println("└  ┴─┘└─┘└┴┘┴─┘└─┘┘└┘└─┘  1.1.8");
         }
 
         return this;
